23f74c2e2872b661aeda0947e4f45590ea6cefd9
modit jain <moditjain121@gmail.com>
2020-08-23 06:34:14 +0000

    clear_configs

--- common.mk
+++ common.mk
@@ -17,11 +17,11 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-du
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-du/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true
