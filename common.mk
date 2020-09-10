f59b50f30f8dcabc0dbae079a5997eb577e59914
Lucchetto <lucchetto.tie@live.com>
2020-09-10 08:43:38 +0200

    sm8150-common: drop qti display mapper
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- common.mk
+++ common.mk
@@ -234,8 +234,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.10 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
-    vendor.qti.hardware.display.allocator@1.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor
+    vendor.qti.hardware.display.allocator@1.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
