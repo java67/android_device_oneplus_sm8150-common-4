f511eb26efdbf5215deeac0d6986a70e303eb96b
Rick M <spookcity138@gmail.com>
2020-09-23 23:14:54 +0200

    sm8150-common: Install vendorcompat verison of protobuf
    Some prebuilt vendor modules contain references to
    libprotobuf-cpp-*.so, but the interface is not stable.  Upgrading
    protobuf would cause those modules to fail, so the vendor version
    of the new protobuf library is renamed to libprotobuf-cpp-*-3.9.1.so.
    Manually install old libprotobuf-cpp-*.so files to /vendor
    to avoid breaking products that have prebuilts that reference them.
    
    Once the new version of protobuf is in each product can be
    inspected for references to libprotobuf-cpp-*.so on /vendor and
    these packages can be included only on those devices that need
    them.

--- common.mk
+++ common.mk
@@ -499,9 +499,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-full-vendorcompat
 
 # Wifi
 PRODUCT_PACKAGES += \
