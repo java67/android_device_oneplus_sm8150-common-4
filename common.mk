43e08ee4dd98059e7be2a65e12a37ee61dfef08f
modit jain <moditjain121@gmail.com>
2020-09-25 09:30:01 +0000

    Revert "sm8150-common: Adjust some camera packages"
    This reverts commit 56f2ce3573f63fddb6e67a1a513bc98cbed29c28.

--- common.mk
+++ common.mk
@@ -163,10 +163,11 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    libdng_sdk \
-    libgui_vendor \
-    libqti-perfd-client \
-    libxml2
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libxml2 \
+    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE
 PRODUCT_PACKAGES += \
